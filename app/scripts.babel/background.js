a7ce9415fa3c02c4645b2f7ecb17d3efb5004dc9
Massimo Galbusera <massimo.galbusera@hotelscan.com>
2019-11-29 11:17:20 +0100

    fix (manifest): don't colorize/strip right away, listen to browserAction

--- app/scripts.babel/background.js
+++ app/scripts.babel/background.js
@@ -4,15 +4,15 @@ chrome.runtime.onInstalled.addListener(details => {
   console.log('previousVersion', details.previousVersion);
 });
 
-chrome.tabs.onUpdated.addListener(tabId => {
+/*chrome.tabs.onUpdated.addListener(tabId => {
   chrome.pageAction.show(tabId);
-});
+});*/
 
 // executes colorize action on button click :)
 chrome.browserAction.onClicked.addListener(tab => {
   // No tabs or host permissions needed!
-  console.log('Stripping ansi colors for ' + tab.url);
+  console.log('Colorizing ansi for ' + tab.url);
   chrome.tabs.executeScript({
-    file: 'scripts/strip.js'
+    file: 'scripts/colorize.js'
   });
 });
